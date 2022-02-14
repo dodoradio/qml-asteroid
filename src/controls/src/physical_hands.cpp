16e3563b5b37f7a9d03f126b8a0b836b7835c224
dodoradio <dodoradio@github.com>
2022-02-14 16:54:58 +0000

    Added initial code for narwhal hands. testing commit. rebase before upstreaming.

---
+++ src/controls/src/physical_hands.cpp
@@ -0,0 +1,200 @@
+#include "physical_hands.h"
+
+PhysicalHands::PhysicalHands()
+{
+
+}
+
+void PhysicalHands::update()
+{
+    if(isEnabled()) {
+        if(m_watchModeTarget)
+        {
+            QFile watch_mode_file("/sys/devices/sop716/watch_mode");
+            if(!watch_mode_file.open(QIODevice::WriteOnly | QIODevice::Text))
+            {
+                qDebug("Unable to open file for write. Check permissions");
+            }
+            QTextStream out(&watch_mode_file);
+            out << 1;
+            qDebug() << "reset hands to watch mode" << parentItem();
+        } else {
+            QFile motor_move_all_file("/sys/devices/sop716/motor_move_all");
+            if(!motor_move_all_file.open(QIODevice::WriteOnly | QIODevice::Text))
+            {
+                qDebug("Unable to open file for write. Check permissions");
+            }
+            QTextStream out(&motor_move_all_file);
+            out << m_hourHandTarget << ":" << m_minuteHandTarget;
+            motor_move_all_file.close();
+            qDebug("moved hands to requested position");
+        }
+    }
+}
+
+int PhysicalHands::hourHandPosition()
+{
+    QFile position("/sys/devices/sop716/position");
+    if (!position.open(QIODevice::WriteOnly | QIODevice::Text))
+        return(0);
+    QTextStream read(&position);
+    return(read.read(7).split(":")[0].toInt());
+}
+
+int PhysicalHands::minuteHandPosition()
+{
+    QFile position("/sys/devices/sop716/position");
+     if (!position.open(QIODevice::WriteOnly | QIODevice::Text))
+        return(0);
+    QTextStream read(&position);
+    return(read.read(7).split(":")[1].toInt());
+}
+
+void PhysicalHands::setHourHandPosition(int newHourHandPosition)
+{
+    m_watchModeTarget = 0;
+    m_hourHandTarget = newHourHandPosition;
+    qDebug() << "hourHandTarget = " << m_hourHandTarget;
+//     QFile motor_move_file("/sys/devices/sop716/motor_move");
+//     if(!motor_move_file.open(QIODevice::WriteOnly | QIODevice::Text))
+//     {
+//         qDebug("Unable to open file for write. Check permissions");
+//     }
+//     QTextStream out(&motor_move_file);
+//     out << "1:" << m_hourHandTarget;
+//     motor_move_file.close();
+    update();
+    qDebug("moved hands to requested position");
+
+}
+
+void PhysicalHands::setMinuteHandPosition(int newMinuteHandPosition)
+{
+    m_watchModeTarget = 0;
+    m_minuteHandTarget = newMinuteHandPosition;
+    qDebug() << "minuteHandTarget = " << m_minuteHandTarget;
+//     QFile motor_move_file("/sys/devices/sop716/motor_move");
+//     if(!motor_move_file.open(QIODevice::WriteOnly | QIODevice::Text))
+//     {
+//         qDebug("Unable to open file for write. Check permissions");
+//     }
+//     QTextStream out(&motor_move_file);
+//     out << "0:" << m_minuteHandTarget;
+//     motor_move_file.close();
+    update();
+    qDebug("moved hands to requested position");
+}
+
+void PhysicalHands::setWatchMode(int newWatchMode = 1)
+{
+    m_watchModeTarget = newWatchMode;
+    if(m_watchModeTarget)
+    {
+        QFile watch_mode_file("/sys/devices/sop716/watch_mode");
+        if(!watch_mode_file.open(QIODevice::WriteOnly | QIODevice::Text))
+        {
+            qDebug("Unable to open file for write. Check permissions");
+        }
+        QTextStream out(&watch_mode_file);
+        out << 1;
+        qDebug("reset hands to watch mode");
+    }
+}
+
+void PhysicalHands::setHourHandPositionStealth(int newHourHandPosition)
+{
+    m_watchModeTarget = 0;
+    m_hourHandTarget = newHourHandPosition;
+    qDebug() << "hourHandTarget = " << m_hourHandTarget;
+    update();
+    qDebug("queued hands move request");
+}
+
+void PhysicalHands::setMinuteHandPositionStealth(int newMinuteHandPosition)
+{
+    m_watchModeTarget = 0;
+    m_minuteHandTarget = newMinuteHandPosition;
+    qDebug() << "minuteHandTarget = " << m_minuteHandTarget;
+    update();
+    qDebug("queued hands move request");
+}
+
+void PhysicalHands::setWatchModeStealth(int newWatchMode = 1)
+{
+    m_watchModeTarget = newWatchMode;
+    if(isEnabled())
+    {
+        if(m_watchModeTarget)
+        {
+            QFile watch_mode_file("/sys/devices/sop716/watch_mode");
+            if(!watch_mode_file.open(QIODevice::WriteOnly | QIODevice::Text))
+            {
+                qDebug("Unable to open file for write. Check permissions");
+            }
+            QTextStream out(&watch_mode_file);
+            out << 1;
+        }
+    }
+    qDebug("queued watch mode change");
+}
+
+
+void PhysicalHands::itemChange(QQuickItem::ItemChange change, const QQuickItem::ItemChangeData &data)
+{
+    QQuickItem::itemChange(change, data);
+    switch (change) {
+    case ItemOpacityHasChanged:
+        if (qFuzzyIsNull(data.realValue))
+            setVisible(false);
+        break;
+    case ItemVisibleHasChanged:
+        //update();
+        break;
+    case ItemEnabledHasChanged:
+        update();
+        break;
+    default:
+        break;
+    }
+}
+
+/*
+void PhysicalHands::setVisible(bool visibility)
+{
+    m_visibility = visibility;
+    qDebug() << "set visibility to " << m_visibility;
+    if(m_visibility) {
+        if(m_watchModeTarget)
+        {
+            QFile watch_mode_file("/sys/devices/sop716/watch_mode");
+            if(!watch_mode_file.open(QIODevice::WriteOnly | QIODevice::Text))
+            {
+                qDebug("Unable to open file for write. Check permissions");
+            }
+            QTextStream out(&watch_mode_file);
+            out << 1;
+            qDebug("reset hands to watch mode");
+        } else {
+            QFile motor_move_all_file("/sys/devices/sop716/motor_move_all");
+            if(!motor_move_all_file.open(QIODevice::WriteOnly | QIODevice::Text))
+            {
+                qDebug("Unable to open file for write. Check permissions");
+            }
+            QTextStream out(&motor_move_all_file);
+            out << m_hourHandTarget << ":" << m_minuteHandTarget;
+            motor_move_all_file.close();
+            qDebug("moved hands to requested position");
+        }
+    }
+}*/
+/*
+bool PhysicalHands::visible()
+{
+    return m_visibility;
+}*/
+/*
+void PhysicalHands::setEffectiveVisibleRecur(bool newEffectiveVisible) {
+    setVisible(newEffectiveVisible);
+    qDebug("setEffectiveVisibleRecur triggered - running setVisible with new value");
+}*/
+
---
+++ src/controls/src/physical_hands.h
@@ -0,0 +1,98 @@
+/*
+ * All rights reserved.
+ *
+ * You may use this file under the terms of BSD license as follows:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the author nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef PHYSICAL_HANDS_H
+#define PHYSICAL_HANDS_H
+
+#include <QtQuick/qquickitem.h>
+#include <QFile>
+#include <QSaveFile>
+
+class PhysicalHands : public QQuickItem
+{
+    Q_OBJECT
+
+    Q_PROPERTY(int hourHandPosition READ hourHandPosition WRITE setHourHandPosition NOTIFY hourHandPositionChanged)
+    Q_PROPERTY(int minuteHandPosition READ minuteHandPosition WRITE setMinuteHandPosition NOTIFY minuteHandPositionChanged)
+    //Q_PROPERTY(int handsPosition[2] Read handsPosition WRITE setHandsPosition NOTIFY handsPositionChanged)
+    Q_PROPERTY(int watchMode READ watchMode WRITE setWatchMode NOTIFY watchModeChanged)
+
+    Q_PROPERTY(int hourHandPositionStealth READ hourHandPosition WRITE setHourHandPositionStealth NOTIFY hourHandPositionChanged)
+    Q_PROPERTY(int minuteHandPositionStealth READ minuteHandPosition WRITE setMinuteHandPositionStealth NOTIFY minuteHandPositionChanged)
+    //Q_PROPERTY(int handsPositionStealth[2] Read handsPositionStealth WRITE setHandsPositionStealth NOTIFY handsPositionChanged)
+    Q_PROPERTY(int watchModeStealth READ watchMode WRITE setWatchModeStealth NOTIFY watchModeChanged)
+
+public:
+    PhysicalHands();
+
+    void setPosition(int,int);
+
+    void setHourHandPosition(int);
+    void setMinuteHandPosition(int);
+
+    int minuteHandPosition();
+    int hourHandPosition();
+
+    void setHandsPosition(int);
+    int* handsPosition();
+
+    void setWatchMode(int);
+    int watchMode();
+
+    //void setVisible(bool);
+
+    //bool visible();
+
+    void setHourHandPositionStealth(int);
+    void setMinuteHandPositionStealth(int);
+
+    void setWatchModeStealth(int);
+
+    //void paint(QPainter *painter) override;
+    //void setEffectiveVisibleRecur(bool);
+    void update();
+
+signals:
+    void hourHandPositionChanged();
+    void minuteHandPositionChanged();
+
+    void watchModeChanged();
+
+    //void visibleChanged();
+protected:
+    void itemChange(ItemChange change, const ItemChangeData &data) override;
+
+private:
+    int m_hourHandTarget = -1;
+    int m_minuteHandTarget = -1;
+    int m_watchModeTarget;
+    bool m_visibility = false;
+
+};
+
+#endif // PHYSICAL_HANDS_H
